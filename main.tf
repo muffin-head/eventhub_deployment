0eea2b30c4cddc14c69aae8858d2186177faa581
Tanmay Kadam <Tanmay.kdm@gmail.com>
2024-12-21 17:34:50 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Event Hub Namespace
 resource "azurerm_eventhub_namespace" "eh_namespace" {
   name                = "ehnamespace-terraform"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = "East US"  # Use the same location as the AKS and ACR deployment
+  resource_group_name = "rg-aks-acr-terraform"  # Replace with the existing resource group name
   sku                 = "Standard"
   capacity            = 1
 
@@ -15,7 +15,7 @@ resource "azurerm_eventhub_namespace" "eh_namespace" {
 resource "azurerm_eventhub" "event_hub" {
   name                = "eventhub-terraform"
   namespace_name      = azurerm_eventhub_namespace.eh_namespace.name
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = "rg-aks-acr-terraform"  # Replace with the existing resource group name
   partition_count     = 2
   message_retention   = 1
 
@@ -28,11 +28,9 @@ resource "azurerm_eventhub" "event_hub" {
 resource "azurerm_eventhub_namespace_authorization_rule" "eh_auth_rule" {
   name                = "RootManageSharedAccessKey"
   namespace_name      = azurerm_eventhub_namespace.eh_namespace.name
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = "rg-aks-acr-terraform"  # Replace with the existing resource group name
   listen              = true
   send                = true
   manage              = true
 }
-
-
 
