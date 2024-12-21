6a17779645bca30e21237bd36d8f28a72747063b
Tanmay Kadam <Tanmay.kdm@gmail.com>
2024-12-21 18:05:01 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "azurerm" {
 
   subscription_id = "1fad5e2e-cd4b-437b-8fff-ee503b4e0e29"
 }
+
 # Event Hub Namespace
 resource "azurerm_eventhub_namespace" "eh_namespace" {
   name                = "sepsisStreamingEventhubNameSpace"
@@ -11,16 +12,14 @@ resource "azurerm_eventhub_namespace" "eh_namespace" {
   resource_group_name = "rg-datalake-terraform"
   sku                 = "Standard"
   capacity            = 1
-
-
 }
 
 # Event Hub with Capture to ADLS
 resource "azurerm_eventhub" "event_hub" {
-  name           = "eventhubsepsisstreaming"
-  namespace_id   = azurerm_eventhub_namespace.eh_namespace.id
-  partition_count = 1
-  message_retention = 1
+  name               = "eventhubsepsisstreaming"
+  namespace_id       = azurerm_eventhub_namespace.eh_namespace.id
+  partition_count    = 1
+  message_retention  = 1
 
   capture_description {
     enabled                 = true
@@ -28,17 +27,16 @@ resource "azurerm_eventhub" "event_hub" {
     interval_in_seconds     = 300
     size_limit_in_bytes     = 314572800
     destination {
-      name       = "EventHubArchive.AzureBlockBlob"
-      storage_account_id = "/subscriptions/1fad5e2e-cd4b-437b-8fff-ee503b4e0e29/resourceGroups/rg-datalake-terraform/providers/Microsoft.Storage/storageAccounts/datalaketfexample"
-      blob_container_name = "eventhub"  # Target ADLS container for Event Hub captures
+      name                = "EventHubArchive.AzureBlockBlob"
+      storage_account_id  = "/subscriptions/1fad5e2e-cd4b-437b-8fff-ee503b4e0e29/resourceGroups/rg-datalake-terraform/providers/Microsoft.Storage/storageAccounts/datalaketfexample"
+      blob_container_name = "eventhub"
       archive_name_format = "eventhub/{Namespace}/{EventHub}/{PartitionId}/{Year}/{Month}/{Day}/{Hour}/{Minute}/{Second}"
     }
   }
-
-
 }
 
-# Shared Access Policy
+# Shared Access Policy (Imported Resource)
+# RootManageSharedAccessKey already exists in Azure and is imported into Terraform
 resource "azurerm_eventhub_namespace_authorization_rule" "eh_auth_rule" {
   name                = "RootManageSharedAccessKey"
   namespace_name      = azurerm_eventhub_namespace.eh_namespace.name
@@ -46,6 +44,8 @@ resource "azurerm_eventhub_namespace_authorization_rule" "eh_auth_rule" {
   listen              = true
   send                = true
   manage              = true
+  lifecycle {
+    # Prevent Terraform from recreating this resource
+    prevent_destroy = true
+  }
 }
-
-
