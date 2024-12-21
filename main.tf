1e86c7441d367d88d94e1f38c4105808249ff198
Tanmay Kadam <Tanmay.kdm@gmail.com>
2024-12-21 17:26:39 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,45 @@
+# Event Hub Namespace
+resource "azurerm_eventhub_namespace" "eh_namespace" {
+  name                = "ehnamespace-terraform"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+  capacity            = 1
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# Event Hub
+resource "azurerm_eventhub" "event_hub" {
+  name                = "eventhub-terraform"
+  namespace_name      = azurerm_eventhub_namespace.eh_namespace.name
+  resource_group_name = azurerm_resource_group.rg.name
+  partition_count     = 2
+  message_retention   = 1
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# Shared Access Policy
+resource "azurerm_eventhub_namespace_authorization_rule" "eh_auth_rule" {
+  name                = "RootManageSharedAccessKey"
+  namespace_name      = azurerm_eventhub_namespace.eh_namespace.name
+  resource_group_name = azurerm_resource_group.rg.name
+  listen              = true
+  send                = true
+  manage              = true
+}
+
+# Output Event Hub Connection String
+output "eventhub_namespace_connection_string" {
+  value = azurerm_eventhub_namespace_authorization_rule.eh_auth_rule.primary_connection_string
+}
+
+# Output Event Hub Name
+output "eventhub_name" {
+  value = azurerm_eventhub.event_hub.name
+}
