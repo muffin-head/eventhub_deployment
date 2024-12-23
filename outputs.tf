37a735c087e8cdcd7fd5d4cab7581d8bac895c8e
Tanmay Kadam <Tanmay.kdm@gmail.com>
2024-12-23 17:53:52 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,1 @@
-output "eventhub_namespace_connection_string" {
-  value     = azurerm_eventhub_namespace.eh_namespace.default_primary_connection_string
-  sensitive = true
-}
 
-output "eventhub_name" {
-  value = azurerm_eventhub.event_hub.name
-}
